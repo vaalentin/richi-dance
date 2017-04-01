fed24a2a1b252ab61ca9a1d7b88da97332b0ff77
Vaalentin <vaalentin@protonmail.com>
2017-04-01 18:13:16 +0200

    Add webpack prod config

---
+++ webpack.prod.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: './dist',
+    filename: '[hash].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: [
+            { loader: 'css-loader' }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: "'production'" }
+    }),
+    new ExtractTextPlugin('[hash].css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        drop_console: true
+      },
+      comments: false,
+      sourceMap: false
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ]
+}
